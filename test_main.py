d3580b889ce1fda0f3bd549e324a308f1812ecbd
JS Ng <ngjunsiang@gmail.com>
2024-01-18 06:41:58 +0000

    [refactor] use autograding module

--- test_main.py
+++ test_main.py
@@ -5,14 +5,13 @@ from autograding import TestInputOutput, TestFunction
 from autograding.case import InOut, FuncCall
 
 
-class TestSF(TestInputOutput):
+class TestNRIC(TestInputOutput):
     def setUp(self):
         self.testcases = [
             InOut(input="T0102051F", output="NRIC is valid."),
             InOut(input="S8521097A", output="NRIC is valid."),
             InOut(input="C1234567C", output="NRIC is invalid."),
             InOut(input="S123456B", output="NRIC is invalid."),
-            InOut(input="0.002300", output="NRIC is invalid."),
         ]
 
 
